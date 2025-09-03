269bd3754945698e585d6aec48020886dc44dc93
Bruh24kz <bruna.buche.lima@escola.pr.gov.br>
2025-09-03 08:52:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function atualizarRelogio() {
+  const agora = new date();
 
+  let horas = agora.getHours().toString().padStart(2, '0');
+  let minutos = agora.getMinutes().toString().padStart(2, '0');
+  let segundos = agora.getSeconds().toString().padStart(2, '0');
+
+  const horaAtual = '${horas}:${minutos}:${segundos}';
+
+  document.getElementbyID('clock').textcontent = horaAtual;
+}
+
+// Atualiza imediatamente e depois a cada segundo 
+atualizarrelogio();
+setInterval (atualizarRelogio, 1000);
